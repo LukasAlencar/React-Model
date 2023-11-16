3f2dec280d37964424d727068228a44fc64ec6a2
lukasalencar <lucas.kauan@aluno.ifsp.edu.br>
2023-11-16 12:06:48 -0300

    First Commit

---
+++ src/pages/IfStyleComponents/IfStyleComponents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Title } from './styles'
+
+const IfStyleComponents = () => {
+  return (
+    <Title>Hello World</Title>
+  )
+}
+
+export default IfStyleComponents
